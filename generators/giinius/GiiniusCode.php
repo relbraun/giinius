813fd724b85326cab0b11175d47dfb61ea9f711d
relbraun <relbraun@gmail.com>
2014-12-29 01:04:40 +0200

    giinius

---
+++ generators/giinius/GiiniusCode.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of GiineiusCode
+ *
+ * @author Kalman
+ */
+class GiiniusCode
+{
+    //put your code here
+}
+
+?>
---
+++ generators/giinius/GiiniusGenerator.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of GiiniusGenerator
+ *
+ * @author Kalman
+ */
+class GiiniusGenerator
+{
+    //put your code here
+}
+
+?>
