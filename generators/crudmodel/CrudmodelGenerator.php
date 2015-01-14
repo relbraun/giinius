12e7c6ab90b54201f3e66fe6f5abc3393e665fd3
relbraun <relbraun@gmail.com>
2015-01-15 00:59:19 +0200

    table creation

--- generators/crudmodel/CrudmodelGenerator.php
+++ generators/crudmodel/CrudmodelGenerator.php
@@ -1,63 +1,70 @@
 <?php
+
 /**
  *
  * @return CWebApplication
  */
-function app(){
+function app()
+{
     return Yii::app();
 }
+
 Yii::import('giin.generators.crudmodel.GiiniusBuilder');
 
 class CrudmodelGenerator extends CodeGenerator
 {
-	public $codeModel='giin.generators.crudmodel.CrudmodelCode';
 
-        public function actionFormBuilder($model_name)
-        {
-            $models= GiiniusBuilder::model()->findAllByAttributes(array('model'=>$model_name));
-            if(!$models){
-                $this->renderPartial('error');
-            }
-            foreach($models as $field){
-                $this->render('formBuilder', array('field' => $field));
-            }
-            $script=<<<EOD
+    public $codeModel = 'giin.generators.crudmodel.CrudmodelCode';
+
+    public function actionFormBuilder($model_name)
+    {
+        $models = GiiniusBuilder::model()->findAllByAttributes(array('model' => $model_name));
+        if (!$models) {
+            $this->renderPartial('error');
+        }
+        foreach ($models as $field) {
+            $this->render('formBuilder', array('field' => $field));
+        }
+        $script = <<<EOD
    $('')
 EOD;
-            $cs=app()->getClientScript();
-            $cs->registerScript(__FILE__, $script);
+        $cs = app()->getClientScript();
+        $cs->registerScript(__FILE__, $script);
+    }
+
+    protected function buildDbTbl()
+    {
+        /** @var CDbConnection $db */
+        $db = Yii::app()->db;
+        $tbl_name = 'giinius_builder';
+
+        if (!in_array($tbl_name, $db->getSchema()->getTableNames())) {
+            $schema = $db->getSchema();
+            $sql = $schema->createTable($tbl_name, array(
+                'id' => 'pk',
+                'model' => 'string',
+                'attribute' => 'string',
+                'field_type' => 'string',
+                'css' => 'string',
+                'options' => 'text',
+                'label' => 'string',
+                'placeholder' => 'boolean',
+            ));
+            $index = $schema->createIndex('KEY_' . $tbl_name, $tbl_name, 'model,attribute', true);
+            $command = $db->createCommand($sql);
+            if (!$command->execute())
+                throw new CDbException('The system didn\'t succeded to build the new table: ' . $tbl_name);
+            if (!$db->createCommand($index)->execute())
+                throw new CException('The system didn\'t succeded to build the new table: ' . $tbl_name);
+            return true;
         }
+        return true;
+    }
 
-        protected function buildDbTbl()
-        {
-	        /** @var CDbConnection $db */
-	        $db=Yii::app()->db;
-            $tbl_name='giinius_builder';
+    protected function beforeAction($action)
+    {
+        if ($this->buildDbTbl())
+            return parent::beforeAction($action);
+    }
 
-            if(!in_array($tbl_name, $db->getSchema()->getTableNames())){
-                $schema=$db->getSchema();
-	            $sql=$schema->createTable($tbl_name,array(
-		            'id'=>'pk',
-		            'model'=>'string',
-		            'attribute'=>'string',
-		            'field_type'=>'string',
-		            'css'=>'string',
-		            'options'=>'text',
-	            ));
-                    $index=$schema->createIndex('KEY_'.$tbl_name, $tbl_name, 'model,attribute', true);
-	            $command=$db->createCommand($sql);
-	            if(!$command->execute())
-                        throw new CDbException('The system didn\'t succeded to build the new table: '.$tbl_name);
-                    if(!$db->createCommand($index)->execute())
-                        throw new CException('The system didn\'t succeded to build the new table: '. $tbl_name);
-                    return true;
-
-            }
-        }
-
-        protected function beforeAction($action)
-        {
-            if($this->buildDbTbl())
-                return parent::beforeAction($action);
-        }
 }
